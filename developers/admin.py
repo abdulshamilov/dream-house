f1ef1d9a12367c28959141a944770d97bc62f392
abdulshamilov <itisabdul21@gmail.com>
2025-11-03 18:43:23 +0300

    админка обнова developers

--- developers/admin.py
+++ developers/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Developer, Subscription
 
-# Register your models here.
+@admin.register(Developer)
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'logo')
+    search_fields = ('name',)
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'developer', 'created_at')
+    list_filter = ('developer', 'created_at')
+    search_fields = ('user__phone_number', 'developer__name')
